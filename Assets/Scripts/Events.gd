632d7ea66c5128f187ae84ac3eed023a27bf8fdd
Victor Vermot <victorvermot@gmail.com>
2025-06-26 10:04:44 +0200

    init

---
+++ Assets/Scripts/Events.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _start_game() -> void:
+	# This function is called when the game starts.
+	# You can add your game initialization logic here.
+	print("Game started!")
