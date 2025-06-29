36e6fa114dd045c56135a7eaeeb95d5091bb9f28
Vermisso <60429652+Vermisso@users.noreply.github.com>
2025-06-29 12:29:04 +0200

    main scene

--- Scenes/activable_lever.gd
+++ Scenes/activable_lever.gd
@@ -3,18 +3,6 @@ extends Activable
 @export var mesh: MeshInstance3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func _ready() -> void:
-	mesh.material_overlay = mesh.material_overlay.duplicate()
-
-func activate_outline() -> void:
-	return
-	if is_active:
-		return
-	mesh.material_overlay.grow = true
-	
-func deactivate_outline() -> void:
-	return
-	mesh.material_overlay.grow = false
 
 func activate() -> void:
 	SoundManager.play_lever_sound()
